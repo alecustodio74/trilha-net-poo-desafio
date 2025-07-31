4e0a09068544de7f28b237f18b3ebc6263684108
Alexandre Custódio <95694176+alecustodio74@users.noreply.github.com>
2025-07-31 11:45:25 -0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,3 +1,20 @@
 ﻿using DesafioPOO.Models;
+using System;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Teste com iPhone:");
+        Smartphone iphone = new Iphone("99999-9999", "iPhone 14", "1234567890", 256);
+        iphone.Ligar();
+        iphone.ReceberLigacao();
+        iphone.InstalarAplicativo("WhatsApp");
+
+        Console.WriteLine("\nTeste com Nokia:");
+        Smartphone nokia = new Nokia("88888-8888", "Nokia Tijolinho", "0987654321", 64);
+        nokia.Ligar();
+        nokia.ReceberLigacao();
+        nokia.InstalarAplicativo("Snake");
+    }
+}
